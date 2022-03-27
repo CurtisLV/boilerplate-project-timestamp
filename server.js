f79e48733daf77e5b66d2f70e106a25818c85398
CurtisLV <curtislv@gmail.com>
2022-03-27 19:49:22 +0300

    final
    Timestamp Microservice final working
    wasnt easy.

--- server.js
+++ server.js
@@ -1,32 +1,36 @@
-// server.js
-// where your node app starts
-
-// init project
-var express = require('express');
-var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/api/:date?', (req, res) => {
+    let date = req.params.date; // vnk inputs, poh kāds
+    let dateVal; //formāts, ko var lietot
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+    if (!isNaN(date)) {
+        date = new Date(parseInt(date));
+    }
+
+    if (!date) {
+        // ja nav nekā
+        date = new Date(); // tad iedodam tagad laiku
+    }
+
+    dateVal = new Date(date);
+
+    if (dateVal.toString() === 'Invalid Date') {
+        return res.json({ error: dateVal.toString() });
+    }
+
+    let dateString = dateVal.toUTCString();
+    let dateUnix = Date.parse(dateVal);
+    res.json({ unix: dateUnix, utc: dateString });
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+    console.log('Your app is listening on port ' + listener.address().port);
 });
